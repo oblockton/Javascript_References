efce8bd2517c6159071ddd09e8141e93af52125d
Omari Blockton <oblockton@gmail.com>
2017-07-18 01:09:03 -0700

    add jQuery files

---
+++ jQuery/jQuer_Selctors.js
@@ -0,0 +1,15 @@
+/*
+jQuery Selectors
+*/
+
+$('tag')
+
+$('.class')
+
+$('#id')
+
+/* Selects DOM node by tag,class, or id
+Example */
+
+var tagList = $('tag') var classList = $('.class') var idList = $('#id')
+//creates a Javascript objects containing all nodes with the selector entered.
